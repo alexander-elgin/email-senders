dfab47e0b906b79caa0070a376131a0ea85d2ee2
Alex Elgin <sascha.elgin@gmail.com>
2021-01-21 16:50:06 +0600

    add an option to send text / html

--- src/engines/sendinblue.js
+++ src/engines/sendinblue.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
-module.exports = ({ apiKey }) => (subject, content, sender, to) => fetch('https://api.sendinblue.com/v3/smtp/email', {
-    body: JSON.stringify({ sender, subject, textContent: content, to }),
+module.exports = ({ apiKey }) => (subject, content, sender, to, type= 'text') => fetch('https://api.sendinblue.com/v3/smtp/email', {
+    body: JSON.stringify({ sender, subject, [type === 'html' ? 'htmlContent' : 'textContent']: content, to }),
     headers: {
         accept: 'application/json',
         'api-key': apiKey,
